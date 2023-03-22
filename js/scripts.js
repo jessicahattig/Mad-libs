2df6b5192fd8b1379d40afc5768e36f54b00752a
jessicahattig <jessicahattig@gmail.com>
2023-03-22 09:15:37 -0700

    Changed scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,6 @@
 window.onload = function() {
-  // first we set up an event handler for the form submission
   let form = document.querySelector("form");
-  form.onsubmit = function(event) {
-    // then we print values to the story area;
-    // we're hardcoding these values for now
+  form.onsubmit = function(event) {  // there's a new parameter
     document.querySelector("span#person1a").innerText = "person 1";
     document.querySelector("span#person1b").innerText = "person 1";
     document.querySelector("span#person1c").innerText = "person 1";
@@ -14,7 +11,9 @@ window.onload = function() {
     document.querySelector("span#noun").innerText = "noun";
     document.querySelector("span#exclamation").innerText = "exclamation";
 
-    // then we show the story by removing the class attribute
     document.querySelector("div#story").removeAttribute("class");
+
+    // we prevent the default refresh action for the submit event
+    event.preventDefault();
   };
 };
