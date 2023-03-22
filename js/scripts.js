ca3a179539302aafca7ec5db88454dc1ea3eb357
jessicahattig <jessicahattig@gmail.com>
2023-03-22 11:20:33 -0700

    Changing input to call caps.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,16 @@
 window.onload = function() {
-  // first we set up an event handler for the form submission
   let form = document.querySelector("form");
   form.onsubmit = function(event) {
-    // then we print values to the story area;
-    // we're hardcoding these values for now
-    document.querySelector("span#person1a").innerText = "person 1";
-    document.querySelector("span#person1b").innerText = "person 1";
-    document.querySelector("span#person1c").innerText = "person 1";
-    document.querySelector("span#person2a").innerText = "person 2";
-    document.querySelector("span#person2b").innerText = "person 2";
-    document.querySelector("span#animal").innerText = "an animal";
-    document.querySelector("span#verb").innerText = "verb";
-    document.querySelector("span#noun").innerText = "noun";
-    document.querySelector("span#exclamation").innerText = "exclamation";
+    // in this section we get the value for each form input
+    const person1Input = document.getElementById("person1Input").value;
+    
 
-    // then we show the story by removing the class attribute
+    // then we set the story variables to the values we got from the form
+    document.querySelector("span#person1a").innerText= person1Input.toUpperCase();
+   
     document.querySelector("div#story").removeAttribute("class");
+
+    
   };
 };
+console.log(Text)
